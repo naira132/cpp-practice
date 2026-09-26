e334c2d197ffc28e1366f47c410b9474b6288ba3
naira_132 <nfaro002@ucr.edu>
2026-09-25 19:43:49 -0700

    Added getline

--- week-1/loops.cpp
+++ week-1/loops.cpp
@@ -5,11 +5,34 @@ using namespace std;
 
 int main(){
 string name;
+string temp;
 
 cin >> name;
 
 for(size_t i = 0; i < name.length(); i++){
     cout << i << ": " << name[i] << endl;
 }
+cin >> temp;
+
+cout << "The length of the string is: " << temp.length() << endl;
+cout << "The first character is: " << temp[0] << endl;
+cout << "The last character is: " << temp[temp.length() - 1] << endl;
+
+
+string test;
+
+cin >> test;
+
+char first = 'J';
+test[0]= first;
+
+cout << test << endl;
+cin.ignore(); 
+string fullName;
+
+getline(cin, fullName);
+
+cout << "Full name: " << fullName << endl;
+
 return 0;
 }
