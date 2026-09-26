b09704b2871b110b4d1668d95299bf3087239594
naira_132 <nfaro002@ucr.edu>
2026-09-26 01:40:32 -0700

    nested loops adde

--- week-1/loops.cpp
+++ week-1/loops.cpp
@@ -34,30 +34,78 @@ int main(){
 
 // cout << "Full name: " << fullName << endl;
 
-int num = 1;
+// int num = 1;
 
-    while (num <= 50) {
-        // 1. Check for FizzBuzz first (divisible by both 3 and 5)
-        if (num % 3 == 0 && num % 5 == 0) {
-            cout << "FizzBuzz" << endl;
-        }
-        // 2. If not FizzBuzz, check for Fizz
-        else if (num % 3 == 0) {
-            cout << "Fizz" << endl;
-        }
-        // 3. If not Fizz, check for Buzz
-        else if (num % 5 == 0) {
-            cout << "Buzz" << endl;
-        }
-        // 4. If none of the above, just print the number
-        else {
-            cout << num << endl;
-        }
+//     while (num <= 50) {
+//         // 1. Check for FizzBuzz first (divisible by both 3 and 5)
+//         if (num % 3 == 0 && num % 5 == 0) {
+//             cout << "FizzBuzz" << endl;
+//         }
+//         // 2. If not FizzBuzz, check for Fizz
+//         else if (num % 3 == 0) {
+//             cout << "Fizz" << endl;
+//         }
+//         // 3. If not Fizz, check for Buzz
+//         else if (num % 5 == 0) {
+//             cout << "Buzz" << endl;
+//         }
+//         // 4. If none of the above, just print the number
+//         else {
+//             cout << num << endl;
+//         }
         
-        num++; // Move to the next number
-    }
+//         num++; // Move to the next number
+//     }
 
+// for(int i =1; i <=10; i++){
+//     cout << i << endl;
+//     if (i==5){
+//         break;
+//     }
+// }
+
+
+
+// string name;
+
+// getline(cin, name);
+
+// for (size_t i = 0; i<name.length(); i++){
+//     if (name[i]==' '){
+//         cout << "Found a space" << endl;
+//     }
+// }
+
+// string name;
+
+// getline(cin, name);
+//     int j =0;
+
+// for (size_t i = 0; i<name.length(); i++){
+//     if (name[i]=='a'){
+//         j++;
+//     }
+
+// }
+// cout << "The number of a's in the string is: " << j << endl;
+
+
+
+// int j = 0;
+// int i =0;
+// while(i <3){
+//     cout << "*";
+//     i++;
+// }
+// cout
+
+// for (int i = 0; i < 3; i++){
+//     cout << "*";
+// for (int j = 0; j < 2; j++){
+//     cout << "*";
+
+// }
+// cout << endl;
+// }
 return 0;
-
-
 }
